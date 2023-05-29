9ac65fac46f2697941ff194d81826a5ebfe0b0b5
thislife_won <77386183+sw8744@users.noreply.github.com>
2023-05-29 22:08:08 +0900

    Update main.py

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ for _ in range(8):
     res.append(temp) # 현재 항생제 내성 세균의 수와 야생 세균의 수를 결과에 저장
 
     for _ in range(anti_germ):
-        integer = r.randint(0, 36)
+        integer = r.randint(0, 35)
         double = r.randint(0, 10)
         num = float(str(str(integer) + '.' + str(double))) # 임의의 소수값 0부터 36까지 소수점 아래 첫째자리까지 표현
         if int(num) - num == 0: # 만일 랜덤한 값이 정수라면(바둑알이 칸 안에 안착했다면)
@@ -17,7 +17,7 @@ for _ in range(8):
                 anti_germ -= 1 # 항생제 내성 세균을 제거
 
     for _ in range(natural_germ):
-        integer = r.randint(0, 36)
+        integer = r.randint(0, 35)
         double = r.randint(0, 10)
         num = float(str(str(integer) + '.' + str(double)))
         if int(num) - num == 0 and arr[integer // 5 - 1][integer % 5 - 1] == 1: # 만일 바둑알이 칸 안에 안착하였고 그 지점이 흰색이라면
